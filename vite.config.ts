7eb9e81c7c9e09ee22b7591b99836747d1406347
pac <sunpochin@gmail.com>
2022-09-09 20:08:33 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/',
-	// base: '/iron-vite-ecommerce/',
+	// base: '/',
+	base: '/iron-vite-ecommerce/',
 	plugins: [vue()],
 	resolve: {
 		alias: {
